d566f8888617c78f69a2aa18275399a952de1d25
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-09-17 14:33:44 +0300

    Editing the login file

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
 <?php
 include("connections.php");
-// $my_email = $_POST['email'];  
-// $password = $_POST['pass'];  
-$query = $mysqli->prepare("SELECT email, password FROM users");
+$my_email = $_POST['email'];  
+$pass = $_POST['password'];  
+$query = $mysqli->prepare("SELECT email, password FROM users WHERE email='$my_email' && password='$pass';");
 $query->execute();
 $array = $query->get_result();
 
