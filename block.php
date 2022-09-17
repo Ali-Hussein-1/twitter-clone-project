5172d10a67c24076e09311b2f369d2d55ccc575b
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-09-17 21:12:45 +0300

    Adding a blocking API

--- block.php
+++ block.php
@@ -3,9 +3,11 @@ include("connections.php");
 $blocker = $_POST["blocker_id"];
 $blocked = $_POST["blocked_id"];
 
-$statement =$mysqli->prepare("INSERT INTO `blocked` (`id`, `password`, `email`, `user_name`, `following count`)
- VALUES (NULL, ?, ?, ?, NULL)");
-$statement->bind_param("sss", $pass, $email, $name );
+$statement =$mysqli->prepare("INSERT INTO `blockers` (`blocker_id`, `blocked_id`) VALUE(?,?)");
+$statement->bind_param("ii", $blocker, $blocked);
 $statement->execute();
-$statement->close();
+$response = [];
+$response["success"] = true;
+
+echo json_encode($response);
 ?>
