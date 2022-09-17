f30ff8852eb2d401097514ec03542eac0151d316
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-09-17 23:23:12 +0300

    Adding headers to the php files

--- block.php
+++ block.php
@@ -1,3 +1,6 @@
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 <?php
 include("connections.php");
 $blocker = $_POST["blocker_id"];
