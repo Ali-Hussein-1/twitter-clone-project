1fca6077d76d0654695f3515d10c3bf5c00a9be7
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-09-17 21:35:32 +0300

    Editing the login API

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
 <?php
 include("connections.php");
-$my_email = $_POST['email'];  
-$pass =hash("sha256" , $_POST["password"]); 
-$query = $mysqli->prepare("SELECT email, password FROM users WHERE email='$my_email' && password='$pass';");
+// $my_email = $_POST['email'];  
+// $pass =hash("sha256" , $_POST["password"]);
+$query = $mysqli->prepare("SELECT email, password FROM users");
 $query->execute();
 $array = $query->get_result();
 
