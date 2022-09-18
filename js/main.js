c0c15493a24af443ef29939f47deb207e7240366
ALichrieff <alichrief12gmail.com>
2022-09-18 23:01:41 +0300

    allow user to display his tweet

--- js/main.js
+++ js/main.js
@@ -1,13 +1,57 @@
 
 
-function getResponse() {
-    fetch("http://localhost/twitter-clone-project/retrieve.php")
-    .then((res) => res.json())
-    .then(data => {
-        console.log(data)
-        
-    })
+
+
+const twTxtContent = document.getElementById("text-content")
+const addTweet = document.getElementById("btn")
+const tweets = document.getElementById("show")
+let newTweetContent = document.createElement("div")
+
+addTweet.onclick = (e) => {
+    console.log("dd")
+  e.preventDefault()
+  newTweetContent.classList.add("new-content")
+  const content=twTxtContent.value
+  newTweetContent.innerHTML = `
+  
+ <div class="tweet">
+          
+            <div class="tweet-detail">
+              <div class="user">
+                <span>NorbertB</span>
+                <span>@NBMenyhart</span>
+                <span >. 4h</span>
+              </div>
+                <div>${content}</div>
+              </div>
+`
+  twTxtContent.value = ""
+  tweets.appendChild(newTweetContent)
 }
-window.addEventListener('load', function () {
-    getResponse()
-})
+
+
+let btn=document.getElementById("btn-clicked")
+
+getResponse=function()
+{
+   
+
+    console.log("hello")
+     const tweet=document.getElementById("text-content").value
+     const formData = new FormData()
+     formData.append("text",tweet)
+    
+     //fetch the data into database 
+     console.log(formData)
+      fetch('http://localhost/twitter-clone-project/send.php', {
+          method: 'POST',
+          body:formData
+      })
+      .then(response =>{
+           response.json() 
+          console.log(response)})
+
+}
+
+btn.addEventListener('click',getResponse)
+
