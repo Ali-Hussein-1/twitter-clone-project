fd5ce958c6f29f960c4559ab802d1438fe5b1cf6
Ali-Hussein-1 <ali.kh.hussein@gmail.com>
2022-09-18 09:07:50 +0300

    Fixing the API's

--- login.php
+++ login.php
@@ -1,7 +1,8 @@
+
+<?php
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
-<?php
 include("connections.php");
 // $my_email = $_POST['email'];  
 // $pass =hash("sha256" , $_POST["password"]);
--- tweet.php
+++ tweet.php
@@ -1,7 +1,8 @@
+
+<?php
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
-<?php
 include("connections.php");
 $tweet = $_POST['text'];
 $photo = $_POST['photo'];
